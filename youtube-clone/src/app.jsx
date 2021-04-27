778f3bdd2f2b14c7e39336a990bdc9b3ff18b926
unknown <ssgsj.carranza@gmail.com>
2021-04-27 15:36:26 -0500

    working on getting video information for related videos

--- youtube-clone/src/app.jsx
+++ youtube-clone/src/app.jsx
@@ -8,7 +8,8 @@ class App extends Component {
     state = {
         videoInfo:[],
         searchVideoTerm:'',
-        videoResultList:''
+        videoResultList:'',
+        selectedVideo: null
     }
 
     handleChange = (event) => {
@@ -22,9 +23,19 @@ class App extends Component {
         event.preventDefault();
         let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${this.state.searchVideoTerm}&key=AIzaSyAQpaNvVuucNcZowsZ6WDwXvsHoUQPI86E`)
         this.setState({
-            videoResultList: response.data.items
+            videoResultList: response.data.items,
         })
+        let responseOne = await axios. get (`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${this.state.selectedVideo}&key=AIzaSyBoQ41RDkdAMnmcOq3cExX1Rx41RW7za3Q`)
         console.log(this.state.videoResultList)
+        console.log(responseOne)
+        console.log(this.state.selectedVideo)
+    }
+
+    mapVidoes(){
+        return this.state.videoResultList.index.map(videoResultList=>
+            <List
+                key={videoResultList.id}
+                videoResultList={videoResultList}/>)
     }
 
     render() {
@@ -32,6 +43,7 @@ class App extends Component {
             <div>
                 <Search handleChange={this.handleChange} onSubmit={this.onSubmit}/>
                 <VideoPlayer />
+                
                 
             </div>
         );
---
+++ youtube-clone/src/components/Video.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Videos = () => {
+    return (
+        <div className="video-list">
+            <div style={{padding: "20px"}}>
+                <h1 style={{textAlign: "center", fontSize: "15px", fontWeight:"bold"}}>
+                    Related Videos
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Videos;
